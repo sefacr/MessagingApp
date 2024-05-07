fe5eb3d0f2af1c282a7eccfec767a576eafee7f4
sefacr <aacarsefa@gmail.com>
2024-05-07 14:22:02 +0300

    login button clicked

--- MessagingApp/LoginViews/LoginViewController.swift
+++ MessagingApp/LoginViews/LoginViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -19,11 +19,32 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         configureUI()
     }
     
     @IBAction func loginBtnClicked(_ sender: Any) {
+        if isValid(email: emailTextField.text!) && passwordTextField.text! != "" {
+            performLogin()
+        } else {
+            showAlert(title: "Login Credentials Invalid", message: "Please enter registered account info.", in: self)
+            return
+        }
+    }
+    
+    func performLogin() {
+        FUser.loginUserWithEmail(email: emailTextField.text!, password: passwordTextField.text!) { (error) in
+            if error != nil {
+                showAlert(title: "Error Signing In", message: "Please try again.", in: self)
+            }else {
+                self.goToTabBar()
+            }
+        }
+    }
+    
+    func goToTabBar(){
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: USER_DID_LOGIN_NOTIFICATION), object: nil, userInfo: [kUSERID : FUser.currentId()])
+        instantiateViewController(identifier: "tabBarController", animated: true, by: self, completion: nil)
     }
     
     
